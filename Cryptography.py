6356808556266ecdc8df3926ce3ccda9d950922a
Saurabh2308 <107534769+Saurabh2308@users.noreply.github.com>
2024-01-12 18:43:06 +0530

    Uploaded

---
+++ Cryptography.py
@@ -0,0 +1,34 @@
+def machine():
+    # creating key strings
+    keys = 'abcdefghijklmnopqrstuvwxyz !'
+    # auto generating the vaules of strings
+    # value will be generted by taking last to first
+    # concatinated with the rest of the string
+    values = keys[-1] + keys[0:-1]
+    # print(keys)
+    # print(values)
+
+    # creating two dictionaries
+    encrytDict = dict(zip(keys, values))
+    decryptDict = dict(zip(values, keys))
+
+    # user input
+    message = input("Enter your secret message: ")
+    mode = input("Choice option : Encode(E) OR Decode(D)")
+
+    #encode and decode
+    if mode.upper() == 'E':
+        newMessage = ''.join([encrytDict[letter] for letter in message.lower()])
+        print("Your encrypted message is :")
+                            
+    elif mode.upper() == 'D':
+        newMessage = ''.join([decryptDict[letter] for letter in message.lower()])
+        print("Your decrypted message is :")
+
+    else:
+        print("Please try again, wrong choice entered")
+
+    return newMessage.capitalize()
+
+
+print(machine())
